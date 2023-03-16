c35adf90c6d679b68f7e1323dfab8ec39bf8d465
hb10 <harishbg357@gmail.com>
2023-03-16 08:31:51 +0530

    provider.tf modified

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIAZQROBB5J2EKFA66A"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "ap-south-1"
 }
